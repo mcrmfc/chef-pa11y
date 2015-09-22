0dce86c768063a7b1fef432ead10ac9bc529676b
Matthew Robbins <mcrobbins@gmail.com>
2015-09-22 12:53:31 +0100

    new mongo cookbook dependency as mongodb3 didn't work happily with older versions of chef-solo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures pa11y'
 long_description 'Installs/Configures pa11y'
-version          '0.1.1'
+version          '0.2.1'
 
 depends 'nodejs'
 depends 'mongodb'
